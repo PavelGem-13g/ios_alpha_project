85fe27d60642b4b7daa2a0966ab7081122242044
hazkerindii228 <59193313+PavelGem-13g@users.noreply.github.com>
2023-10-08 15:04:56 +0300

    create xcode project

---
+++ project/project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Павел on 08.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
